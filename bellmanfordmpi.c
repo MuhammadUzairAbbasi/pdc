767688ae85ddb03d997271d4a6c6965a944e262a
MuhammadUzairAbbasi <136907020+MuhammadUzairAbbasi@users.noreply.github.com>
2023-11-21 22:32:53 +0500

    Update bellmanfordmpi.c

--- bellmanfordmpi.c
+++ bellmanfordmpi.c
@@ -54,8 +54,88 @@ int print_result(bool has_negative_cycle, int *dist) {
 }
 
 void bellman_ford(int my_rank, int p, MPI_Comm comm, int n, int *mat, int *dist, bool *has_negative_cycle) {
-    // Implementation of the Bellman-Ford algorithm remains unchanged
-    // ...
+   int loc_n; // need a local copy for N
+    int loc_start, loc_end;
+    int *loc_mat; //local matrix
+    int *loc_dist; //local distance
+
+    //step 1: broadcast N
+    if (my_rank == 0) {
+        loc_n = n;
+    }
+    MPI_Bcast(&loc_n, 1, MPI_INT, 0, comm);
+
+    //step 2: find local task range
+    int ave = loc_n / p;
+    loc_start = ave * my_rank;
+    loc_end = ave * (my_rank + 1);
+    if (my_rank == p - 1) {
+        loc_end = loc_n;
+    }
+
+    //step 3: allocate local memory
+    loc_mat = (int *) malloc(loc_n * loc_n * sizeof(int));
+    loc_dist = (int *) malloc(loc_n * sizeof(int));
+
+    //step 4: broadcast matrix mat
+    if (my_rank == 0)
+        memcpy(loc_mat, mat, sizeof(int) * loc_n * loc_n);
+    MPI_Bcast(loc_mat, loc_n * loc_n, MPI_INT, 0, comm);
+
+    //step 5: bellman-ford algorithm
+    for (int i = 0; i < loc_n; i++) {
+        loc_dist[i] = INF;
+    }
+    loc_dist[0] = 0;
+    MPI_Barrier(comm);
+
+    bool loc_has_change;
+    int loc_iter_num = 0;
+    for (int iter = 0; iter < loc_n - 1; iter++) {
+        loc_has_change = false;
+        loc_iter_num++;
+        for (int u = loc_start; u < loc_end; u++) {
+            for (int v = 0; v < loc_n; v++) {
+                int weight = loc_mat[utils::convert_dimension_2D_1D(u, v, loc_n)];
+                if (weight < INF) {
+                    if (loc_dist[u] + weight < loc_dist[v]) {
+                        loc_dist[v] = loc_dist[u] + weight;
+                        loc_has_change = true;
+                    }
+                }
+            }
+        }
+        MPI_Allreduce(MPI_IN_PLACE, &loc_has_change, 1, MPI_CXX_BOOL, MPI_LOR, comm);
+        if (!loc_has_change)
+            break;
+        MPI_Allreduce(MPI_IN_PLACE, loc_dist, loc_n, MPI_INT, MPI_MIN, comm);
+    }
+
+    //do one more step
+    if (loc_iter_num == loc_n - 1) {
+        loc_has_change = false;
+        for (int u = loc_start; u < loc_end; u++) {
+            for (int v = 0; v < loc_n; v++) {
+                int weight = loc_mat[utils::convert_dimension_2D_1D(u, v, loc_n)];
+                if (weight < INF) {
+                    if (loc_dist[u] + weight < loc_dist[v]) {
+                        loc_dist[v] = loc_dist[u] + weight;
+                        loc_has_change = true;
+                        break;
+                    }
+                }
+            }
+        }
+        MPI_Allreduce(&loc_has_change, has_negative_cycle, 1, MPI_CXX_BOOL, MPI_LOR, comm);
+    }
+
+    //step 6: retrieve results back
+    if(my_rank == 0)
+        memcpy(dist, loc_dist, loc_n * sizeof(int));
+
+    //step 7: remember to free memory
+    free(loc_mat);
+    free(loc_dist);
 }
 
 int main(int argc, char **argv) {
